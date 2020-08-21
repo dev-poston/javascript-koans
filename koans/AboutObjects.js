6b2bd385362672395413a9ff3e3322e8fead02d0
Devon Poston <dev.poston@gmail.com>
2020-08-20 22:20:06 -0700

    fix case sen test

--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -13,7 +13,7 @@ describe('About Objects', function() {
 
     it('should confirm that properties are case sensitive', function() {
       expect(meglomaniac.henchwoman).toBe('Harley');
-      expect(meglomaniac.henchWoman).toBe(FILL_ME_IN);
+      expect(meglomaniac.henchWoman).toBe(undefined);
     });
   });
 
